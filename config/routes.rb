b6fc57a99e86b0cf7345f2dfc04f201522c03fc8
kennchong <kennchong85@gmail.com>
2020-03-17 15:32:41 +0800

    completed the controller except the user_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
   devise_for :user
   root to: 'pages#home'
   resources :flats do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:show, :new, :create]
     resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :flats do
-    resources :bookings, only: [:new, :create]
-    resources :reviews, only: [:new, :create]
-  end
 end
