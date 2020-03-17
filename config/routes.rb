42ef9ae20415b1c881b56a1897065f99d78ef601
Maxime Trinquet <maxime.trinquet@gmail.com>
2020-03-17 15:16:44 +0800

    Controller Finished + navbar + color in variable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
+
   devise_for :user
   root to: 'pages#home'
   resources :flats do
@@ -8,9 +7,4 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :flats do
-    resources :bookings, only: [:new, :create]
-    resources :reviews, only: [:new, :create]
-  end
 end
