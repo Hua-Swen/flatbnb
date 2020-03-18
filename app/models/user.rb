a4b21fac68f99874411c20c718095425cb2033d6
tan hua swen <tan.huaswen@gmail.com>
2020-03-18 10:57:37 +0800

    added cloudinary for user and flats

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   has_many :flats
+  has_one_attached :photo
   has_many :bookings
   has_many :reviews
   # Include default devise modules. Others available are:
