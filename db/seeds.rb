28cd270abeb089e51ea634c5a0a1729c2f1456ef
tan hua swen <tan.huaswen@gmail.com>
2020-03-18 13:19:50 +0800

    quick edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-require 'pry-byebug'
 
 puts "destroying all databases"
 Amenity.destroy_all
